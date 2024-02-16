2c71d9e210484b796f5c7831a27fe0b70b623c1f
juman2312 <jumanhassan47@gmail.com>
2024-02-16 11:06:57 -0800

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+
+void bubble_sort(int *array, size_t size);
+{
+	int temp;
+	size_t i= 0;
+	size_t j;
+
+	if(!array || !size)
+		return;
+
+	while (i < size)
+	{
+		for (j = 0, j < size, j++)
+		{
+			if(array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array.size);
+			}
+		}
+		i++;
+	}
+
+}
