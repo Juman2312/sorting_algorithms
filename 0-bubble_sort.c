9af665731aeed44d808f64d74f00e5233af5536f
juman2312 <jumanhassan47@gmail.com>
2024-02-17 10:14:27 -0800

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,7 @@
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order using the
- *		Bubble Sort algorithm.
+ *               Bubble Sort algorithm.
  *
  * @array: Pointer to the array to be sorted.
  * @size: Size of the array.
@@ -10,28 +10,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	int temp;
-	size_t i, j;
-	int swapped;
+	size_t i = 0;
+	size_t j;
 
-	if (!array || size < 2)
+	if (!array || !size)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	while (i < size)
 	{
-		swapped = 0;
-
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
+				print_array(array, size);
 			}
 		}
-
-		if (swapped == 0)
-			break;
+		i++;
 	}
 }
