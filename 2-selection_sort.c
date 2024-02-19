3e65aa72e8be5d86a5fd5ee05174f56ac43d69e2
juman2312 <jumanhassan47@gmail.com>
2024-02-19 02:09:27 -0800

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,18 +14,22 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL)
 		return;
 
-	for (i = 0; i < size - 1; i++) {
+	for (i = 0; i < size - 1; i++)
+	{
 		min_idx = i;
 		swaps = 0;
 
-		for (j = i + 1; j < size; j++) {
-			if (array[j] < array[min_idx]) {
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			{
 				min_idx = j;
 				swaps++;
 			}
 		}
 
-		if (min_idx != i) {
+		if (min_idx != i)
+		{
 			temp = array[i];
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
