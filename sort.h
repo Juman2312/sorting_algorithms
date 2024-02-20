0a756eda4449fa76792b7da20c2b3ceb1d8ff37d
juman2312 <jumanhassan47@gmail.com>
2024-02-20 10:14:56 -0800

    cocktail

--- sort.h
+++ sort.h
@@ -32,6 +32,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 void merge(int *array, int *tmp, int start, int mid, int end);
 void merge_sort_recursive(int *array, int *tmp, int start, int end);
 void merge_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
+void cocktail_sort_list(listint_t **list);
 
 
 #endif
